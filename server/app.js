cdcd845ac9645da1e122d0139cb2b1a2d7cd7ac3
Vanessa Ating <atingenkay@gmail.com>
2017-10-25 15:57:06 +0100

    Modify files

--- server/app.js
+++ server/app.js
@@ -10,12 +10,14 @@ app.use(logger('dev'));
 
 // Parse incoming requests data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format
-app.all('*', (req, res) => {
-  res.status(404)
-    .send("The page you requested doesn't exist");
+app.get('*', (req, res) => {
+  res.status(200).send({ message: 'Welcome to nothingness' });
 });
 
 export default app;
+
