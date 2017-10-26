31b62dcb53e3dce21ca3f3f4a457b27d3eb5b704
Vanessa Ating <atingenkay@gmail.com>
2017-10-26 01:39:12 +0100

    Create api endpoint to modify recipe

--- server/controllers/controlRecipe.js
+++ server/controllers/controlRecipe.js
@@ -66,6 +66,36 @@ class Recipe {
    *
    * @param {any} req
    * @param {any} res
+   * @returns {json} updates an existing recipe
+   * @memberof Recipe
+   */
+  updateRecipe(req, res) {
+    const query = req.params.recipeId;
+    if (!query) {
+      return res.status(400)
+        .send('Recipe parameter is needed');
+    }
+    for (let i = 0; i < db.recipes.length; i += 1) {
+      if (parseInt(db.recipes[i].id, 10) === parseInt(req.params.recipeId, 10)) {
+        db.recipes[i].name = req.body.newName || db.recipes[i].name;
+        db.recipes[i].ingredients = [req.body.newIngredients] || db.recipes[i].ingredients;
+        db.recipes[i].description = req.body.newDescription || db.recipes[i].description;
+        return res.status(200)
+          .json({
+            status: 'success',
+            message: 'Recipe modified',
+            recipe: db.recipes[i]
+          });
+      }
+    }
+    return res.status(404)
+      .send(`recipe with id ${query} not found`);
+  }
+  /**
+   *
+   *
+   * @param {any} req
+   * @param {any} res
    * @returns {json} the result from the api
    * @memberof Recipe
    */
