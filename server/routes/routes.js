6329ab18d50647a152351bc13a7cf18c26c1e363
Vanessa Ating <atingenkay@gmail.com>
2017-10-26 03:01:14 +0100

    Create file to hold different routes for the api endpoints

---
+++ server/routes/routes.js
@@ -0,0 +1,16 @@
+import * as Recipe from '../controllers/controlRecipe';
+
+
+const recipe = new Recipe.default();
+
+
+const router = (app) => {
+  app.get('/', (req, res) => {
+    res.status(200)
+      .send('Welcome to more-recipes api');
+  });
+  app.post('/api/recipes', recipe.addRecipe);
+};
+
+export default router;
+
