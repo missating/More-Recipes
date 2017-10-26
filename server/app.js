15e86b1f57fdf05fa4c253dbd06af4222ce647c5
Vanessa Ating <atingenkay@gmail.com>
2017-10-26 03:00:21 +0100

    Modify file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import router from './routes/routes';
 
 // Set up the express app
 const app = express();
@@ -13,6 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+router(app);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format
 app.get('*', (req, res) => {
