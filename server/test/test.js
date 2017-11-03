d252a12cbca04f3eff08d4d01f5d408879602154
Vanessa Ating <atingenkay@gmail.com>
2017-11-03 08:11:52 +0100

    modify file

--- server/test/test.js
+++ server/test/test.js
@@ -1,144 +1,198 @@
 process.env.NODE_ENV = 'test';
 
-
 import chaiHttp from 'chai-http';
 import chai from 'chai';
+import jwt from 'jsonwebtoken';
 import app from '../app';
-import db from '../models/index';
-import * as passwordHelper from '../validation/encrypt';
 
-const helper = new passwordHelper.default();
-
-const { expect } = chai;
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+const userSignup = '/api/v1/users/signup';
+const userSignin = '/api/v1/users/signin';
+let user = {};
+let data = {};
+let userToken;
 
-describe('Test for API', () => {
-  db.User.destroy({
-    cascade: true,
-    truncate: true,
-    restartIdentity: true
-  });
-  after((done) => {
-    db.User.destroy({ where: { id: { $notIn: [1, 2, 3] } } });
-    db.Recipe.destroy({ where: { id: { $notIn: [1, 2, 3] } } });
-    done();
-  });
-  describe('encrypted password', () => {
-    it('should return false for invalid password', () => {
-      expect(helper.decrypt('hsdvjkshnvnkew', 'hhvcjw.fjkbwbh.wrjbfjrw')).to.equal(false);
+describe('API Endpoints testing', () => {
+  describe('Create a new User.', () => {
+    beforeEach(() => {
+      user = {
+        username: 'fidelis',
+        email: 'abc@gmail.com',
+        password: 'password12345',
+      };
     });
-  });
-
-  describe('GET /', () => {
-    it('Should return 200', (done) => {
+    it('Should return 201 for successfull user creation.', () => {
       chai.request(app)
-        .get('/')
+      .post(userSignup)
+        .send(user)
         .end((err, res) => {
-          expect(res).to.have.status(200);
-          done();
+          expect(res.status).to.equal(201);
         });
     });
-    it('Should return 404 for unknown routes', (done) => {
-      chai.request(app)
-        .get('/some/very/stupid/route')
+    it('Should return 400 for an already existing email.', () => {
+      const sameData = Object.assign({}, user);
+      chai.request(app).post(userSignup)
+        .send(sameData)
         .end((err, res) => {
-          expect(res).to.have.status(404);
-          done();
+          expect(res.status).to.equal(400);
         });
     });
-    it('Should return 404 for posts to undefined routes', (done) => {
-      chai.request(app)
-        .post('/some/undefined/route')
-        .send({ random: 'random' })
+    it('Should return 400 if no email is passed.', () => {
+      const noEmail = Object.assign({}, user);
+      delete noEmail.email;
+      chai.request(app).post(userSignup)
+        .send(noEmail)
         .end((err, res) => {
-          expect(res).to.have.status(404);
-          done();
+          expect(res.status).to.equal(400);
         });
     });
-    it('Should return 404 for get requests without token', (done) => {
-      chai.request(app)
-        .get('/api/recipes/user/all')
+    it('Should return 400 if no password is entered.', () => {
+      const noPassword = Object.assign({}, user);
+      delete noPassword.password;
+      chai.request(app).post(userSignup)
+        .send(noPassword)
         .end((err, res) => {
-          expect(res).to.have.status(404);
-          done();
+          expect(res.status).to.equal(400);
         });
     });
-  });
-  describe('POST recipe', () => {
-    it('Should return 403 for post requests without token', (done) => {
-      chai.request(app)
-        .post('/api/recipes')
-        .send({
-          name: 'fried chicken',
-          ingredients: 'friene, food, ajksk',
-          description: 'description',
-        })
+    it('Should return 400 if no username is provided.', () => {
+      const noUsername = Object.assign({}, user);
+      delete noUsername.username;
+      chai.request(app).post(userSignup)
+        .send(noUsername)
         .end((err, res) => {
-          expect(res).to.have.status(403);
-          done();
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 400 for an existing username.', () => {
+      const sameusername = Object.assign({}, user);
+      chai.request(app).post(userSignup)
+        .send(sameusername)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
         });
     });
   });
-  describe('API to Get all recipes', () => {
-    it('Should return 200', (done) => {
-      chai.request(app)
-        .get('/api/recipes')
+  describe('User sign in', () => {
+    beforeEach(() => {
+      user = {
+        email: 'sudoku@gmail.com',
+        password: '123456',
+      };
+    });
+    it('Should return 201 for a successful login', () => {
+      chai.request(app).post(userSignin)
+        .send(user)
         .end((err, res) => {
-          expect(res).to.have.status(200);
-          done();
+          userToken = res.body.token;
+          expect(res.status).to.equal(201);
+        });
+    });
+    it('Should return 400 if no email is provided on sign in', () => {
+      const noEmail = Object.assign({}, user);
+      delete noEmail.email;
+      chai.request(app).post(userSignin)
+        .send(noEmail)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 400 if no password is provided on sign in', () => {
+      const noPassword = Object.assign({}, user);
+      delete noPassword.password;
+      chai.request(app).post(userSignin)
+        .send(noPassword)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 403 for a wrong password', () => {
+      const wrongPassword = Object.assign({}, user);
+      wrongPassword.password = 'veryWrongPassword';
+      chai.request(app).post(userSignin)
+        .send(wrongPassword)
+        .end((err, res) => {
+          expect(res.status).to.equal(403);
+        });
+    });
+    it('Should return 403 for an incorrect email address', () => {
+      const wrongEmail = Object.assign({}, user);
+      wrongEmail.email = 'verywrongemail@yahoo.com';
+      chai.request(app).post(userSignin)
+        .send(wrongEmail)
+        .end((err, res) => {
+          expect(res.status).to.equal(403);
         });
     });
   });
-  it('Should return 200 for sorted recipes', (done) => {
-    chai.request(app)
-      .get('/api/recipes?sort=up&order=des')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-});
-describe('API to update recipe', () => {
-  it('Should return 403 for unauthorized', (done) => {
-    chai.request(app)
-      .put('/api/recipes/1')
-      .send({
-        name: 'A new name',
-        ingredients: 'Some ingredients',
-        description: 'some new description'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(403);
-        done();
-      });
-  });
-});
-describe('Test for User', () => {
-  it('should not allow unregistered user sign in', (done) => {
-    chai.request(app)
-      .post('/api/users/signin')
-      .send({
-        email: 'unenkay@gmail.com',
-        password: 'others'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(400);
-        done();
-      });
-  });
-  it('should not allow invalid credentials sign in', (done) => {
-    chai.request(app)
-      .post('/api/users/signin')
-      .send({
-        email: '1234',
-        password: 'something'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(500);
-        expect(res.body).to.have.property('message').equal('Email and password don\'t match');
-        done();
-      });
+  describe('Add Recipe', () => {
+    beforeEach(() => {
+      data = {
+        name: 'Ghana Jollof',
+        description: 'Best Jollof in the world',
+        category: 'Lunch',
+        ingredients: ['Rice', 'Tomatoes'],
+        instructions: ['Cook this shii well'],
+      };
+    });
+    it('Should return 403 if no token is supplied before adding a recipe', () => {
+      chai.request(app).post('/api/v1/recipes')
+        .send(data)
+        .end((err, res) => {
+          expect(res.status).to.equal(403);
+        });
+    });
+    it('Should return 400 if no recipe name is provided.', () => {
+      const noName = Object.assign({}, data);
+      delete noName.name;
+      chai.request(app).post('/api/v1/recipes')
+        .send(noName)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 400 if no description is provided', () => {
+      const noDescription = Object.assign({}, data);
+      delete noDescription.description;
+      chai.request(app).post('/api/v1/recipes')
+        .send(noDescription)
+        .end((err, res) => {
+          userToken = res.body.token;
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 400 if no category is provided', () => {
+      const noCategory = Object.assign({}, data);
+      delete noCategory.category;
+      chai.request(app).post('/api/v1/recipes')
+        .send(noCategory)
+        .end((err, res) => {
+          userToken = res.body.token;
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 400 if no Ingredient is provided', () => {
+      const noIngredient = Object.assign({}, data);
+      delete noIngredient.ingredients;
+      chai.request(app).post('/api/v1/recipes')
+        .send(noIngredient)
+        .end((err, res) => {
+          userToken = res.body.token;
+          expect(res.status).to.equal(400);
+        });
+    });
+    it('Should return 400 if no Instruction is provided', () => {
+      const noInstruction = Object.assign({}, data);
+      delete noInstruction.instructions;
+      chai.request(app).post('/api/v1/recipes')
+        .send(noInstruction)
+        .end((err, res) => {
+          userToken = res.body.token;
+          expect(res.status).to.equal(400);
+        });
+    });
   });
 });
