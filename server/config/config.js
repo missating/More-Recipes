15405d12295e0c4ffb45fd90ffc89752fc3536e5
Vanessa Ating <atingenkay@gmail.com>
2017-12-11 18:08:03 +0100

    Add sequelize to the dependency file to enable test pass on travis

--- server/config/config.js
+++ server/config/config.js
@@ -6,14 +6,14 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'postgres'
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_TEST,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'postgres'
   },
   production: {
     connection_uri: process.env.DATABASE_URL,
